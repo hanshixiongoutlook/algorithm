acc1f3b2871cb6735b3c0689dbb21f9c673b9ee4
hanshixiong6 <hanshixiong6@jd.com>
2021-12-03 18:30:24 +0800

    update

---
+++ src/main/java/hans/leetcode/tree/M_1361_ValidateBinaryTreeNodes.java
@@ -0,0 +1,58 @@
+package hans.leetcode.tree;
+
+
+import hans.common.pojo.TreeNode;
+import hans.common.utils.Logger;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
+/**
+ */
+public class M_1361_ValidateBinaryTreeNodes {
+
+    @Test
+    public void test() {
+//        1,-1,3,-1    2,-1,-1,-1
+//        1,-1,3,-1    2,3,-1,-1
+        boolean b = validateBinaryTreeNodes(2, new int[]{1, 0}, new int[]{-1,-1});
+        Logger.log(b);
+    }
+
+    public boolean validateBinaryTreeNodes(int n, int[] leftChild, int[] rightChild) {
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        Map<Integer, TreeNode> map = new HashMap<>();
+        TreeNode root = new TreeNode(0);
+        queue.offer(root);
+        map.put(0, root);
+        for (int i=0; i<n; i++) {
+            if (queue.isEmpty()) {
+                return false;
+            }
+            TreeNode next = queue.poll();
+            if (leftChild[i]>=0) {
+                if (map.containsKey(leftChild[i])) {
+                    return false;
+                }
+                next.left = new TreeNode(leftChild[i]);
+                queue.offer(next.left);
+                map.put(next.left.val, next.left);
+            }
+            if (rightChild[i]>=0) {
+                if (map.containsKey(rightChild[i])) {
+                    return false;
+                }
+                next.right = new TreeNode(rightChild[i]);
+                queue.offer(next.right);
+                map.put(next.right.val, next.right);
+            }
+        }
+        return true;
+    }
+
+
+}
