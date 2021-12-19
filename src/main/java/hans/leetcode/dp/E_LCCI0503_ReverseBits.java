4806f96ca5acdcbb2c088f0bece9c98b68602295
hanshixiong <hanshixiong@outlook.com>
2021-12-19 23:39:46 +0800

    add

--- src/main/java/hans/leetcode/dp/E_LCCI0503_ReverseBits.java
+++ src/main/java/hans/leetcode/dp/E_LCCI0503_ReverseBits.java
@@ -9,63 +9,32 @@ import java.util.List;
 public class E_LCCI0503_ReverseBits {
     @Test
     public void test() {
-        int num = reverseBits(-1);
+        int n = 1<<3;
+        Logger.log(n);
+        int num = reverseBits(1);
         Logger.log(num);
     }
 
+    /**
+     * TODO 位运算
+     * @param num
+     * @return
+     */
     public int reverseBits(int num) {
-        if (num==0) {
-            return 1;
-        }
-        Integer[] integers = convertBitCounter(num);
-        if (integers.length==1) {
-            return integers[0]+1;
-        }
-        int max = 0;
-        int count = 0;
-        for (int i=0; i<integers.length; i++) {
-            if (integers[i]<0) {
-                if (integers[i]==-1) {
-                    count = ((i-1)>=0?integers[i-1]:0)+((i+1)<integers.length?integers[i+1]:0) + 1;
-                } else if (integers[i]<-1) {
-                    count = Math.max((i-1)>=0?integers[i-1]:0,(i+1)<integers.length?integers[i+1]:0)+1;
-                }
-                max = Math.max(count, max);
+        int cur = 0;
+        int insert = 0;
+        int res = 1;
+        for(int i = 0; i < 32; i++){
+            if((num & (1 << i)) != 0){
+                cur += 1;
+                insert += 1;
+            }else{
+                insert = cur + 1;
+                cur = 0;
             }
+            res = Math.max(res , insert);
         }
-        return max;
-    }
-
-    public Integer[] convertBitCounter(int num) {
-
-        List<Integer> list = new LinkedList<>();
-        int zeroCounter = 0;
-        int oneCounter = 0;
-        while(num>0) {
-            if (num%2==0) {
-                zeroCounter--;
-                if (oneCounter!=0) {
-                    list.add(oneCounter);
-                }
-                oneCounter=0;
-            } else {
-                oneCounter++;
-                if (zeroCounter!=0) {
-                    list.add(zeroCounter);
-                }
-                zeroCounter=0;
-            }
-            num=num/2;
-        }
-        if (zeroCounter!=0) {
-            list.add(zeroCounter);
-        }
-        if (oneCounter!=0) {
-            list.add(oneCounter);
-        }
-        Integer[] arr = new Integer[list.size()];
-        list.toArray(arr);
-        return arr;
+        return res;
     }
 
 }
---
+++ src/main/java/hans/leetcode/dp/E_LCCI1716Masseuse.java
@@ -0,0 +1,35 @@
+package hans.leetcode.dp;
+
+import hans.common.utils.Logger;
+import org.junit.Test;
+
+public class E_LCCI1716Masseuse {
+    @Test
+    public void test() {
+        int num = massage(new int[]{1, 2, 3, 4, 5});
+        Logger.log(num);
+    }
+
+    /**
+     * f(n) = arr[n] n=0
+     * f(n) = Max(arr[0],arr[1]) n=1
+     * f(n) = max(f(n-1)+arr[n]-arr[n-1], f(n-1)) n>1
+     *
+     * @param nums
+     * @return
+     */
+    public int massage(int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int dp0 = 0, dp1 = nums[0];
+        for (int i = 1; i < n; ++i) {
+            int tdp0 = Math.max(dp0, dp1); // 计算 dp[i][0]
+            int tdp1 = dp0 + nums[i]; // 计算 dp[i][1]
+            dp0 = tdp0; // 用 dp[i][0] 更新 dp_0
+            dp1 = tdp1; // 用 dp[i][1] 更新 dp_1
+        }
+        return Math.max(dp0, dp1);
+    }
+}
