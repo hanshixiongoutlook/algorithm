a5ae1068aa22bf24383b68228ee608baf77fd537
hanshixiong <hanshixiong@outlook.com>
2022-04-05 15:48:59 +0800

    add

--- src/main/java/hans/leetcode/tree/path/E_0257_BinaryTreePaths.java
+++ src/main/java/hans/leetcode/tree/path/E_0257_BinaryTreePaths.java
@@ -8,8 +8,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * TODO
- * https://leetcode-cn.com/problems/binary-tree-paths/
+ Given the root of a binary tree, return all root-to-leaf paths in any order.
+
+ A leaf is a node with no children.
+
+
+ Example 1:
+
+
+ Input: root = [1,2,3,null,5]
+ Output: ["1->2->5","1->3"]
+
+
+ Example 2:
+
+
+ Input: root = [1]
+ Output: ["1"]
+
+
+
+ Constraints:
+
+
+ The number of nodes in the tree is in the range [1, 100].
+ -100 <= Node.val <= 100
+
+ Related Topics 树 深度优先搜索 字符串 回溯 二叉树 👍 700 👎 0
+
  */
 public class E_0257_BinaryTreePaths {
 
--- src/main/java/hans/leetcode/tree/path/M_0113_PathSum.java
+++ src/main/java/hans/leetcode/tree/path/M_0113_PathSum.java
@@ -10,7 +10,48 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
- * TODO 前序、后续兼顾
+ Given the root of a binary tree and an integer targetSum, return all root-to-
+ leaf paths where the sum of the node values in the path equals targetSum. Each
+ path should be returned as a list of the node values, not node references.
+
+ A root-to-leaf path is a path starting from the root and ending at any leaf
+ node. A leaf is a node with no children.
+
+
+ Example 1:
+
+
+ Input: root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+ Output: [[5,4,11,2],[5,8,4,5]]
+ Explanation: There are two paths whose sum equals targetSum:
+ 5 + 4 + 11 + 2 = 22
+ 5 + 8 + 4 + 5 = 22
+
+
+ Example 2:
+
+
+ Input: root = [1,2,3], targetSum = 5
+ Output: []
+
+
+ Example 3:
+
+
+ Input: root = [1,2], targetSum = 0
+ Output: []
+
+
+
+ Constraints:
+
+
+ The number of nodes in the tree is in the range [0, 5000].
+ -1000 <= Node.val <= 1000
+ -1000 <= targetSum <= 1000
+
+ Related Topics 树 深度优先搜索 回溯 二叉树 👍 710 👎 0
+
  */
 public class M_0113_PathSum {
 
